0d0705c4d24939b1b6a664dbfbc165efdcc23556
Dex <90233177+barnazaka@users.noreply.github.com>
2026-03-21 10:37:24 +0100

    Update managed-api.ts

--- nightscore-cli/src/managed-api.ts
+++ nightscore-cli/src/managed-api.ts
@@ -1,1 +1,47 @@
+import {
+  deployContract,
+  findDeployedContract,
+} from '@midnight-ntwrk/midnight-js-contracts';
+import { ContractAddress } from '@midnight-ntwrk/midnight-js-types';
+import { createWitnesses, CreditProfile, DEFAULT_CREDIT_PROFILE } from './witnesses.js';
+import { Contract } from '../contract/managed/nightscore_contract.cjs';
 
+const contract = new Contract(createWitnesses(DEFAULT_CREDIT_PROFILE));
+
+export type NightScoreProviders = {
+  privateStateProvider: any;
+  publicDataProvider: any;
+  zkConfigProvider: any;
+  walletProvider: any;
+  midnightProvider: any;
+};
+
+export const deployNightScore = async (
+  providers: NightScoreProviders,
+  creditProfile: CreditProfile
+) => {
+  const witnesses = createWitnesses(creditProfile);
+  const contractWithWitnesses = new Contract(witnesses);
+
+  return await deployContract(providers, {
+    contract: contractWithWitnesses,
+    privateStateKey: 'nightscore',
+    initialPrivateState: creditProfile,
+  });
+};
+
+export const joinNightScore = async (
+  providers: NightScoreProviders,
+  contractAddress: string,
+  creditProfile: CreditProfile
+) => {
+  const witnesses = createWitnesses(creditProfile);
+  const contractWithWitnesses = new Contract(witnesses);
+
+  return await findDeployedContract(providers, {
+    contractAddress: contractAddress as ContractAddress,
+    contract: contractWithWitnesses,
+    privateStateKey: 'nightscore',
+    initialPrivateState: creditProfile,
+  });
+};
